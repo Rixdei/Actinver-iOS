d03a1342039eaaea2a377edc498e9f58d2a2927f
raymundo.piedra <raymundo.pescador@gruposellcom.com>
2015-06-10 06:56:34 -0500

    Initial Commit

---
+++ Actinver_iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Actinver_iOS
+//
+//  Created by Raymundo Pescador Piedra on 10/06/15.
+//  Copyright (c) 2015 Sellcom Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Actinver_iOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Actinver_iOS
+//
+//  Created by Raymundo Pescador Piedra on 10/06/15.
+//  Copyright (c) 2015 Sellcom Solutions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
